1319f9aed045d3072ce1a1fa1991eaa2e473a5ba
Caio Tarifa <caio@formaweb.com.br>
2015-11-17 16:54:36 -0200

    Configuration Pony to send emails.

--- sssmt.rb
+++ sssmt.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
-require 'tempfile'
 require 'premailer'
+require 'pony'
 
 get '/' do
   @files = Dir.glob('views/mailers/*.{erb}').map {|file| file.split('/').last.gsub('.erb', '')}
@@ -12,12 +12,29 @@ get '/:name' do |name|
 end
 
 get '/premailer/:name' do |name|
-  # Make It Better
-  premailer = Premailer.new(request.base_url + '/' + name, warn_level: Premailer::Warnings::SAFE)
+  premailer = premailer_it name
   premailer.to_inline_css
 end
 
 
 get '/sendmail/:name' do |name|
-  erb name
+  premailer = premailer_it name
+
+  Pony.mail({
+    to: 'sender@example.com',
+    subject: '[TEST] Just a test',
+    body: premailer.to_plain_text,
+    html_body: premailer.to_inline_css,
+    via: :smtp,
+    via_options: {
+      address: 'smtp.gmail.com',
+      port: '587',
+      enable_starttls_auto: true,
+      user_name: 'yourmail@gmail.com',
+      password: '',
+      authentication: 'plain'
+    }
+  })
+
+  erb 'Sended! :D'
 end
